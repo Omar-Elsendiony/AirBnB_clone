f72c1b17c6c3e7fe8f1cdecf475c4d6477229ed4
Omar Elsendiony <omar.sendiony@gmail.com>
2024-01-27 17:02:58 +0200

    re organized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AirBnB_clone
 Welcome to the AirBnB clone project!
+
+# Description
+First step: Write a command interpreter to manage your AirBnB objects.
+This is the first step towards building your first full web application: the AirBnB clone. This first step is very important because you will use what you build during this project with all other following projects: HTML/CSS templating, database storage, API, front-end integration
--- models/base_model.py
+++ models/base_model.py
@@ -1,30 +1,57 @@
 #!/usr/bin/python3
+from typing import Any
 import uuid
 import datetime
 
 class BaseModel:
 
     def __init__(self, *args, **kwargs):
+        # self.updated_at = None
         if (kwargs):
             datetimeFormat = "%Y-%m-%dT%H:%M:%S.%f"
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key == "created_at" or key == "updated_at":
                         dateValue = datetime.datetime.strptime(value, datetimeFormat)
-                        setattr(self, key, dateValue)
+                        setattr(self, "__" + key, dateValue)
                     else:
-                        setattr(self, key, value)
+                        setattr(self, "__" + key, value)
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.__id = str(uuid.uuid4())
+            todayDate = datetime.datetime.now()
+            self.created_at = todayDate.isoformat()
+            self.updated_at = todayDate.isoformat()
+
+    @property
+    def id(self):
+        return self.__id
+
+    @id.setter
+    def id(self, id):
+        self.__id = id
+
+    @property
+    def updated_at(self):
+        return self.__updated_at
+
+    @updated_at.setter
+    def updated_at(self, time):
+        self.__updated_at = time
+
+    @property
+    def created_at(self):
+        return self.__updated_at
     
+    @created_at.setter
+    def created_at(self, time):
+        self.__created_at = time
+
     def __str__(self):
         return f'{self.__class__.__name__} {self.id} {self.__dict__}'
 
     def save(self):
         self.updated_at = datetime.datetime.now()
-    
+
     def to_dict(self):
         dictionaryToBeReturned = self.__dict__
         dictionaryToBeReturned.update({"class": self.__class__.__name__})
