94743ee357394b37300cd182ba90345dcaa744b2
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-13 15:08:53 +0200

    me

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -10,6 +10,25 @@ class test_Place(unittest.TestCase):
         u = Place()
         u.save()
 
+    def test_name(self):
+        u = Place()
+        f = u.name
+        self.assertIsInstance(f, str)
+
+    def test_city_id(self):
+        u = Place()
+        n = u.city_id
+        self.assertIsInstance(n, str)
+
+    def test_user_id(self):
+        u = Place()
+        n = u.user_id
+        self.assertIsInstance(n, str)
+
+    def test_description(self):
+        u = Place()
+        e = u.description
+        self.assertIsInstance(e, str)
 
 if __name__ == '__main__':
     unittest.main()
