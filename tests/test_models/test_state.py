b91ccc53a4b62a347b6c8d6eee7a021868907122
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-13 15:01:20 +0200

    maybe

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,4 @@
 from models.state import State
-from models.base_model import BaseModel
 import unittest
 
 
@@ -10,13 +9,19 @@ class test_State(unittest.TestCase):
         self.assertEqual(state.name, "California")
 
     def test_save(self):
-            u = State()
-            u.save()
+        u = State()
+        u.save()
 
     def test_to_dict(self):
         u = State()
         d = u.to_dict()
         self.assertIsInstance(d, dict)
+    
+    def test_update(self):
+        s = State()
+        print(s)
+        # self.assertIn("name", State.name)
+        
 
 if __name__ == '__main__':
     unittest.main()
