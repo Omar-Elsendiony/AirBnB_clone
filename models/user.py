4d4b2584ef9e0d155033d8df7d3a75ec241715ba
Omar Elsendiony <omar.sendiony@gmail.com>
2024-02-13 09:44:27 +0200

    of

--- models/user.py
+++ models/user.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 """
-    base model module
+    user model module
 """
-from base_model import BaseModel
+from models.base_model import BaseModel
 
 class User(BaseModel):
     """
